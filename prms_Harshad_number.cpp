a9ef2a1f6cb12538d820e468a42975329384dd64
Denver LEE <43673933+AlmightyDenver@users.noreply.github.com>
2019-12-16 14:30:42 +0900

    Add files via upload

---
+++ prms_Harshad_number.cpp
@@ -0,0 +1,16 @@
+//#include <stdio.h>
+//#include <stdbool.h>
+//#include <stdlib.h>
+//
+//bool solution(int x) {
+//	bool answer = true;
+//	int n = 0;
+//	int tmp = x;
+//	while (tmp > 10) {
+//		n += tmp % 10;
+//		tmp /= 10;
+//	}
+//	n += tmp;
+//	if ((x % n) != 0) answer = false;
+//	return answer;									
+//}		
